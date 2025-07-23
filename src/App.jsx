1a60f6340322ca1dabd5982d89ac2ff192c7f0e2
Swetha-k623 <hariswe2731@gmail.com>
2025-07-23 21:58:35 +0530

    Fix: base path for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Movies from "./components/Movies";
 import Watchlist from "./components/Watchlist";
 import Banner from "./components/Banner";
 import { Routes, Route } from "react-router-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 function App() {
@@ -14,59 +13,52 @@ function App() {
     let newWatchlist = [...watchlist, movieObj];
     localStorage.setItem("moviesApp", JSON.stringify(newWatchlist));
     setWatchlist(newWatchlist);
-    console.log(newWatchlist);
   };
 
   let handleRemoveFromWatchlist = (movieObj) => {
     let filteredWatchlist = watchlist.filter((movie) => {
-      return movie.id != movieObj.id;
+      return movie.id !== movieObj.id;
     });
 
     setWatchlist(filteredWatchlist);
     localStorage.setItem("moviesApp", JSON.stringify(filteredWatchlist));
-    console.log(filteredWatchlist);
   };
 
   useEffect(() => {
     let moviesFromLocalstorage = localStorage.getItem("moviesApp");
-    if (!moviesFromLocalstorage) {
-      return;
-    }
+    if (!moviesFromLocalstorage) return;
     setWatchlist(JSON.parse(moviesFromLocalstorage));
   }, []);
 
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
+      <Navbar />
 
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Banner />
-                <Movies
-                  watchlist={watchlist}
-                  handleAddtoWatchlist={handleAddtoWatchlist}
-                  handleRemoveFromWatchlist={handleRemoveFromWatchlist}
-                />
-              </>
-            }
-          />
-
-          <Route
-            path="/watchlist"
-            element={
-              <Watchlist
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Banner />
+              <Movies
                 watchlist={watchlist}
-                setWatchlist={setWatchlist}
+                handleAddtoWatchlist={handleAddtoWatchlist}
                 handleRemoveFromWatchlist={handleRemoveFromWatchlist}
               />
-            }
-          />
-        </Routes>
-      </BrowserRouter>
+            </>
+          }
+        />
+        <Route
+          path="/watchlist"
+          element={
+            <Watchlist
+              watchlist={watchlist}
+              setWatchlist={setWatchlist}
+              handleRemoveFromWatchlist={handleRemoveFromWatchlist}
+            />
+          }
+        />
+      </Routes>
     </>
   );
 }
