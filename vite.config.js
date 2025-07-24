d3d8f35315ee8c1f97d8289dd0d93ab5ed32078f
Swetha-k623 <hariswe2731@gmail.com>
2025-07-24 12:56:30 +0530

    Configured for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/imdb-clone/', 
   plugins: [react()],
-  base: '/imdb-clone/',
 })
+
 
